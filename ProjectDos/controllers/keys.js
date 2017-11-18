c4e73252b3252f621ae5d8b296066eafa54bf85a
Timothy Thompsen <TimmyThompsen@Timothys-MacBook-Pro.local>
2017-11-18 13:10:16 -0800

    Commiting MVC arch

---
+++ ProjectDos/controllers/keys.js
@@ -0,0 +1,16 @@
+console.log("this is loaded");
+
+exports.twitterKeys = {
+	consumer_key: 'xAgJnHgFNaGKnOM4fmkjvCd0q',
+	consumer_secret: 'gh7UkuNmIL3G9IFYBEUbSl7oSXh5kfGykoY9X0w14DM0dUHIgd',
+	access_token_key: '914654394230214656-Lf93ByLvcLhBqX9U9Q399G0gv30rCAE',
+	access_token_secret: 'UQOPcBBpIYLgudTmhG7fl6sWtVWvcEHA1Yr7FgajUJnWC'
+}
+
+exports.facebookKeys = {
+	
+}
+
+exports.instaKeys = {
+	
+}
---
+++ ProjectDos/models/user.js
@@ -0,0 +1,26 @@
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [3]
+      }
+    },
+    email: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      len: [5]
+    },
+    facebook_name: {
+      type: DataTypes.STRING
+    },
+    twitter_name: {
+      type: DataTypes.STRING
+    },
+    insta_name: {
+      type: DataTypes.STRING
+    }
+  });
+  return User;
+};
