73c7cba6e75609d0556788e76e57c4fabe76b505
Gail McFadden <gailmcfadden@Gails-MacBook-Pro.local>
2017-11-20 13:52:35 -0800

    third

--- ProjectDos/public/setting.js
+++ ProjectDos/public/setting.js
@@ -25,85 +25,16 @@ $(function() {
 		  });
 		});
 
-	  // Function for Specilization Input
-	  $( function() {
-	    var availableTags = [
-	      "ActionScript",
-	      "AppleScript",
-	      "Asp",
-	      "BASIC",
-	      "C",
-	      "C++",
-	      "Clojure",
-	      "COBOL",
-	      "ColdFusion",
-	      "Erlang",
-	      "Fortran",
-	      "Groovy",
-	      "Haskell",
-	      "Java",
-	      "JavaScript",
-	      "Lisp",
-	      "Perl",
-	      "PHP",
-	      "Python",
-	      "Ruby",
-	      "Scala",
-	      "Scheme"
-	    ];
-	    $( "#tags" ).autocomplete({
-	      source: availableTags
-	    });
-	  } );
-
-	  // Function for Designation Input
-	  $( function() {
-	    var availableTags = [
-	      "Analyst L1",
-	      "Analyst L2",
-	      "Senior Analyst",
-	      "UI Developer L1",
-	      "UI Developer L2",
-	      "Senior UI Developer",
-	      "Graphics Designer L1",
-	      "Graphics Designer L2",
-	      "Senior Graphics Designer",
-	    ];
-	    $( "#designation" ).autocomplete({
-	      source: availableTags
-	    });
-	  } );
+		//thank you
 
 		$('form').submit(function(){
 			$('.thanks').show();
 			$('.thanks').delay(2000).fadeOut();
 			window.setInterval(function() {
-	     	window.location.reload();
-	     	$('form input#name').focus();
+			 	window.location.reload();
+			 	$('form input#name').focus();
 	 		}, 2500);
-			event.preventDefault(); // Here triggering stops
+			event.preventDefault(); 
 		});
 
-		// Autocomplete for Specialization
-		$( "#tags" ).autocomplete({
-	    source:tags,
-	    //To select only from the autocomplete
-        change: function (event, ui) {
-          if (ui.item == null || ui.item == undefined) {
-            $(this).val("");
-            $(this).attr("disabled", false);
-          }
-        }
-		 });
-
-		// Autocomplete for Designation
-		$( "#designation" ).autocomplete({
-	    source:tags,
-	    //To select only from the autocomplete
-        change: function (event, ui) {
-          if (ui.item == null || ui.item == undefined) {
-            $(this).val("");
-            $(this).attr("disabled", false);
-          }
-        }
-		 });
+		
--- ProjectDos/test/controllersTest.js
+++ ProjectDos/test/controllersTest.js
@@ -12,6 +12,3 @@ describe('pullingTweets', function() {
 
 
 
-//expect(function () {}).to.not.throw();
-//expect({a: 1}).to.not.have.property('b');
-//expect([1, 2]).to.be.an('array').that.does.not.include(3);
